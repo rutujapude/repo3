38d31618503afc033a7d71c2c8cdabc71b1174aa
rutujapude <rutujapude1121@gmail.com>
2020-08-20 16:16:28 +0530

    added example of string and if condition

--- gitexample/src/gitexample/firstname.java
+++ gitexample/src/gitexample/firstname.java
@@ -4,5 +4,18 @@ public class firstname {
 
 public static void main(String[] args) {
 	System.out.println("hello");
+	String name="rutuja";
+	System.out.println("the length of string is:-" +name.length());
+	System.out.println("the uppercase is;-" +name.toUpperCase());
+	System.out.println("the lowercase is :-" +name.toLowerCase());
+	System.out.println(name.indexOf("u"));
+	int t1=18;
+	if(t1<20) {
+		System.out.println("time is smaller than 20");
+	}
+	else {
+		System.out.println("Time is larger than 20 ");
+	}
+	
 }
 }
